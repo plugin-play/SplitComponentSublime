92297f907dd5b77efe0ea0eccf820b8273ed8237
Victor Eduardo Barreto <victorbarreto@W096790>
2017-03-24 20:48:15 -0300

    [up] tests of layout

--- SplitComponents.py
+++ SplitComponents.py
@@ -3,12 +3,12 @@ import sublime_plugin
 
 class SplitComponents(sublime_plugin.EventListener):
 
-	# type files
+	# type files ###Extension 'md' is used for test
 	extensions = ['vue', 'ng', 'md']
 	# original layout
-	original_layout = {'cells': [[0, 0, 1, 1]], 'rows': [0.0, 1.0], 'cols': [0.0, 1.0]}
+	original_layout = sublime.active_window().get_layout()
 	# SplitComponents layout
-	layout = {"cols": [0.0, 0.5, 1.0],"rows": [0.0, 0.7, 1.0],"cells": [[0, 0, 2, 1], [0, 1, 1, 2], [1, 1, 2, 2]]}
+	layout = {"cols": [0.0, 0.5, 1.0],"rows": [0.0, 0.6, 1.0],"cells": [[0, 0, 2, 1], [0, 1, 1, 2], [1, 1, 2, 2]]}
 
 	# show console message
 	print("............::::::::| SplitComponents | Started |::::::::............")
@@ -28,30 +28,41 @@ class SplitComponents(sublime_plugin.EventListener):
 
 	# controller
 	def handle(self):
-		print("handle")
+		print("debug handle")
+
+		# flag to indicate known extension
+		known_extension = False
+
 		try:
 
 			# verify available extensions
 			for extension in self.extensions:
-
+				print(sublime.active_window().get_layout())
 				try:
 					#verify if is a known type file
-					if extension == sublime.active_window().extract_variables()['file_extension']:
+					if extension == sublime.active_window().extract_variables()['file_extension'] and self.layout != sublime.active_window().get_layout():
 						print("debug setting layout")
-						sublime.active_window().set_layout(self.layout)
-						break
 
-					if self.original_layout != sublime.active_window().get_layout():
-						print("debug set original layout")
-						sublime.active_window().set_layout(self.original_layout)
-
-						# set new original layout
+						# save actual layout
 						self.original_layout = sublime.active_window().get_layout()
+
+						# set Split layout
+						sublime.active_window().set_layout(self.layout)
+
+						# set new files in the tabs and set names
+						known_extension = True
 						break
 
 				except KeyError as error:
+					print(error)
 					print("Error to change layout")
 
+			# if isn't a known extension and split layout is set, back to original layout
+			if False == known_extension and self.layout == sublime.active_window().get_layout():
+				print("debug set original layout")
+				sublime.active_window().set_layout(self.original_layout)
+
 		except KeyError as error:
+			print(error)
 			print("Error to recognize file extension")
 
