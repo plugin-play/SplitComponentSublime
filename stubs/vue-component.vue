e9179e65469e2259dd010054633761b99c140d86
Antério Vieira <anteriovieira@gmail.com>
2017-03-28 18:31:15 -0300

    Added vue component stub

---
+++ stubs/vue-component.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card">something</div>
+</template>
+
+<script>
+  export default {
+    name: 'VueComponent'
+  }
+</script>
+
+<style>
+  .card {
+    padding: 10px;
+    border: 1px solid #e8e8e8;
+  }
+</style>
