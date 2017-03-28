fa3906e8fa834e9c3a6e7ad787d55aa1f2d637da
Victor Eduardo Barreto <victorbarreto@W096790>
2017-03-28 16:01:49 -0300

    [up] Many comments for @TODO in the code : )  - Implemented partition of file in js css and html

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # SplitComponents
 A Plugin Sublime Text
+
+<script>
+	$(1);
+	$(2);
+	$(3);
+</script>
+
+<html>
+	<h1>1</h1>
+	<h1>2</h1>
+	<h1>3</h1>
+</html>
+
+<style>
+	border: 1px red solid;
+	border: 2px red solid;
+	border: 3px red solid;
+</style>
--- SplitComponents.py
+++ SplitComponents.py
@@ -5,10 +5,19 @@ class SplitComponents(sublime_plugin.EventListener):
 
 	# type files ###Extension 'md' is used for test
 	extensions = ['vue', 'ng', 'md']
+	# extens? usada nos arquivos abertos nas abas, quando houver o split
+	group_extension = '.sc'
 	# original layout
 	original_layout = sublime.active_window().get_layout()
+
+	# nome e extensão do arquivo original .vue ou .ng
+	original_file = {'name':'', 'ext':''}
+
 	# SplitComponents layout
 	layout = {"cols": [0.0, 0.5, 1.0],"rows": [0.0, 0.6, 1.0],"cells": [[0, 0, 2, 1], [0, 1, 1, 2], [1, 1, 2, 2]]}
+
+	# conteúdo do arquivo .vue ou .ng
+	file_content = {'js':[], 'html':[], 'css':[]}
 
 	# show console message
 	print("............::::::::| SplitComponents | Started |::::::::............")
@@ -35,31 +44,100 @@ class SplitComponents(sublime_plugin.EventListener):
 
 		try:
 
-			# verify available extensions
-			for extension in self.extensions:
-				print(sublime.active_window().get_layout())
-				try:
-					#verify if is a known type file
-					if extension == sublime.active_window().extract_variables()['file_extension'] and self.layout != sublime.active_window().get_layout():
-						print("debug setting layout")
+			# @TODO
+			# fazer uma verificação se a extensao do arquivo aberto é diferente aos arquivos abertos nas abas (.sc)
+			if sublime.active_window().extract_variables()['file_extension'] != self.group_extension:
 
-						# save actual layout
-						self.original_layout = sublime.active_window().get_layout()
+			# se sim, não entra para mudar a tela, se não é necessáio:
+			# - Salvar o conteúdo das 3 abas em uma variável
+			# - Apagar o conteúdo do arquivo original .vue ou .ng
+			# - Injetar o conteúdo das 3 abas que está na variavel e salvar
+			# - Fechar sem salvar os 3 arquivos que est? um em cada aba
+			# - Retirar o split de 3 grupos (abas), deixando somente 1
 
-						# set Split layout
-						sublime.active_window().set_layout(self.layout)
+				# verify available extensions
+				for extension in self.extensions:
+					# print(sublime.active_window().get_layout())
 
-						# set new files in the tabs and set names
-						known_extension = True
-						break
+					try:
+						#verify if is a known type file
+						if extension == sublime.active_window().extract_variables()['file_extension'] and self.layout != sublime.active_window().get_layout():
+							# print("debug setting layout")
 
-				except KeyError as error:
-					print(error)
-					print("Error to change layout")
+							# @TODO talvez não será preciso guardar o layout anterior
+							# save actual layout
+							self.original_layout = sublime.active_window().get_layout()
+
+							# @TODO
+							# Salvar o nome do arquivo atual numa variáel ou num dicionario
+							# Salvar a extensão do arquivo atual numa variável ou dicionario
+							self.original_file['name'] = sublime.active_window().extract_variables()['file_base_name']
+							self.original_file['ext'] = sublime.active_window().extract_variables()['file_extension']
+
+							# Ler o arquivo atual e salvar em uma variavel
+							file = open(sublime.active_window().extract_variables()['file'], 'r')
+							content = file.readlines()
+
+							js = None
+							html = None
+							css = None
+							# print(content)
+
+							# Identificar as partes no arquivo html, css e js
+							for linha in content:
+
+								# @TODO ajustar essas flags para um modelo mais moderno, ficou muito extenso
+								#  Flags para ajudar na separacao do arquivo
+								if linha.find('<html>') >= 0 :
+									html =  True
+
+								if linha.find('</html>') >= 0 :
+									html =  False
+
+								if linha.find('<style>') >= 0:
+									css = True
+
+								if linha.find('/<style>') >= 0:
+									css = False
+
+								if linha.find('<script>') >= 0:
+									js = True
+
+								if linha.find('/<script>') >= 0:
+									js = False
+
+								# Salva as linhas separadamente
+								if True == js:
+									self.file_content['js'].append(linha)
+
+								if True == html:
+									self.file_content['html'].append(linha)
+
+								if True == css:
+									self.file_content['css'].append(linha)
+
+							file.close()
+
+							# Fechar o arquivo atual
+
+							# Fazer o split na tela 3 grupos
+							sublime.active_window().set_layout(self.layout)
+
+							# Criar 3 arquivos com a extensao .sc (splitcomponents). Um em cada group (aba). Os arquivos dao nome a aba
+							# Injetar os dados que foram separados, um em cada aba
+
+							# @TODO talvez n? preciso guardar na flag se ja tem um arquivo do tipo .vue, .ng aberto
+							# set new files in the tabs and set names
+							known_extension = True
+							break
+
+					except KeyError as error:
+						print(error)
+						print("Error to change layout")
 
 			# if isn't a known extension and split layout is set, back to original layout
 			if False == known_extension and self.layout == sublime.active_window().get_layout():
-				print("debug set original layout")
+				# print("debug set original layout")
 				sublime.active_window().set_layout(self.original_layout)
 
 		except KeyError as error:
